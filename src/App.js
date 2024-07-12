287df5bcaaaa8f8cf595cd69f3c93dbffe20d130
Linet Ndau <mkandoelinet@gmail.com>
2024-07-12 21:51:17 +0300

    added files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Video from './Video';
 import Project from './Project';
 import Podcast from './Podcast';
 import Content from './content';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./Landingpage/landingpage";
+import Portfolio from "./Portfolios/portfolio";
+import Portfolio5 from "./Aline";
+import Portfolio4 from "./Daisy";
+import Portfolio3 from "./Wato";
+import Portfolio2 from "./Kur";
+import Portfolio1 from "./Linet";
 
 
 function App() {
@@ -15,6 +23,17 @@ function App() {
 <Project/>
 <Podcast/>
 <Content/>
+<Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+    </Router>
+    <Portfolio1/>
+        <Portfolio2/>
+        <Portfolio3/>
+        <Portfolio4/>
+        <Portfolio5/>
 
     </div>
 
